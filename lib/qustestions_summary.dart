91407666c74c4827e79b600bc19dc8b454112144
AlexanderBabak <bbkshow1987@gmail.com>
2023-10-03 10:13:09 +0300

    upload project

---
+++ lib/qustestions_summary.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              var isCorrectAnswer =
+                  data['user_answer'] == data['correct_answer'];
+
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    margin: const EdgeInsets.only(right: 20),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: isCorrectAnswer
+                          ? const Color.fromARGB(255, 54, 255, 255)
+                          : const Color.fromARGB(255, 239, 53, 230),
+                    ),
+                    child: Center(
+                      child: Text(
+                        ((data['question_index'] as int) + 1).toString(),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['question'] as String,
+                          style: GoogleFonts.lato(
+                            color: const Color.fromARGB(255, 249, 224, 255),
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          data['user_answer'] as String,
+                          style: GoogleFonts.lato(
+                            color: const Color.fromARGB(255, 182, 86, 255),
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          data['correct_answer'] as String,
+                          style: GoogleFonts.lato(
+                            color: const Color.fromARGB(255, 129, 144, 255),
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
